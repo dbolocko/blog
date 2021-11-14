446aff55f11fb8fb3733daabfbd545c0f6e14b90
dbolocko <35933445+dbolocko@users.noreply.github.com>
2021-11-14 16:10:18 +0300

    added sharing post button
    ... and personally post  page

---
+++ app/listOfPosts.py
@@ -0,0 +1,18 @@
+numOfLastPost=3
+listOfPosts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!',
+            'num': '3'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!',
+            'num':'2'
+        }, 
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!',
+            'num':'1'
+        }
+    ]
--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,9 @@
 from flask import render_template, redirect
 from app import app
 from app.push_post import Push_post
+from app.listOfPosts import listOfPosts
 
-listOfPosts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }, 
-        {
-            'author': {'username': 'Ипполит'},
-            'body': 'Какая гадость эта ваша заливная рыба!!'
-        }
-    ]
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -25,9 +13,16 @@ def index():
 def newpost():
     form=Push_post()
     if form.validate_on_submit():
+        
         listOfPosts.reverse()
         listOfPosts.append({'author': {'username': form.username.data},
-            'body': form.p_text.data})
+            'body': form.p_text.data,'num':''+str(len(listOfPosts)+1)})
         listOfPosts.reverse()
         return redirect('/index')
     return render_template('push_text.html', form=form)
+
+@app.route('/post/<numofpost>')
+def viewPost(numofpost):
+    p1=listOfPosts[len(listOfPosts)-int(numofpost)]
+    return render_template('post.html', post=p1)
+
